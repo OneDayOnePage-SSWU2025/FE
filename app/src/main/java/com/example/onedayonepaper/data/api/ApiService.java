01f6b452d0e8dbc27a53d8f04c6ab0f11f4749f1
양진선 <yanggi200@gmail.com>
2025-11-25 21:31:33 +0900

    [feat] 북리포트 확인 api 연동
    읽은 책 수랑 메모 수 확인 가능함다

--- app/src/main/java/com/example/onedayonepaper/data/api/ApiService.java
+++ app/src/main/java/com/example/onedayonepaper/data/api/ApiService.java
@@ -4,6 +4,7 @@ import com.example.onedayonepaper.LoginActivity;
 import com.example.onedayonepaper.data.dto.HomeResponse;
 import com.example.onedayonepaper.data.dto.LoginRequest;
 import com.example.onedayonepaper.data.dto.LoginResponse;
+import com.example.onedayonepaper.data.dto.ReportResponse;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
@@ -16,4 +17,7 @@ public interface ApiService {
     Call<HomeResponse> getHomeGroups();
     @POST("/users/login")
     Call<LoginResponse> login(@Body LoginRequest request);
+
+    @GET("/users/report")
+    Call<ReportResponse> getReport();
 }
