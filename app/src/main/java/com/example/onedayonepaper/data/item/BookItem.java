57f3458ad8773c05f0311e3e411c16f5aa7ab37c
88guri <quffl2002@naver.com>
2025-11-27 15:54:48 +0900

    [Feat] /groups/detail 관련 Item 추가

---
+++ app/src/main/java/com/example/onedayonepaper/data/item/BookItem.java
@@ -0,0 +1,15 @@
+package com.example.onedayonepaper.data.item;
+
+public class BookItem {
+    private int bookId;
+    private String imgUrl;
+    private String bookTitle;
+    private String author;
+    private String createdDate;
+
+    public int getBookId() { return bookId; }
+    public String getImgUrl() { return imgUrl; }
+    public String getBookTitle() { return bookTitle; }
+    public String getAuthor() { return author; }
+    public String getCreatedDate() { return createdDate; }
+}
