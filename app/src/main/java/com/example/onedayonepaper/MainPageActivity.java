d2967fa3b1a47f95c4af121b494463e41bd8e28b
양진선 <yanggi200@gmail.com>
2025-12-11 17:07:03 +0900

    [refact] 사용자 로그인 및 로그아웃 개선
    자동 로그인 기능 추가
    로그인 시 패스워드 텍스트 숨김 처리

--- app/src/main/java/com/example/onedayonepaper/MainPageActivity.java
+++ app/src/main/java/com/example/onedayonepaper/MainPageActivity.java
@@ -1,6 +1,8 @@
 package com.example.onedayonepaper;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +20,16 @@ public class MainPageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        SharedPreferences prefs = getSharedPreferences("auth", Context.MODE_PRIVATE);
+         String savedToken = prefs.getString("jwt", null);
+
+         if (savedToken != null && !savedToken.isEmpty()) {
+             //토큰 존재 시 자동 로그인
+             Intent intent = new Intent(this, MainActivity.class);
+             startActivity(intent);
+         }
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_page);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
